f10fe5fa99d8a29f640584679e22262c2def1595
iqdxa <1429316040@qq.com>
2023-03-05 14:49:42 +0800

    初步完成结算相关代码

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # StoreSimulator
+## Introduction
+This is a game called store simulator. I want to improve my C skill by coding this project.   
--- StoreSimulator/StoreSimulator.c
+++ StoreSimulator/StoreSimulator.c
@@ -1,25 +1,28 @@
 #include <stdio.h>
 #define MAX_COMMODITY_KIND 30
+#define MAX_NAME_LENGTH 10
+
+//TODO Add commodities by users
+//TODO save trade history
 
 typedef struct commodity
 {
 	unsigned int commodityID;
-	char commodityName[10];
+	char commodityName[MAX_NAME_LENGTH];
 	double commodityPrice;
 	int commodityNum;		//Remaining quantity of goods
-
 }COMMODITY;
 
 typedef struct effect
 {
 	int effectID;
-	char effectName[10];
+	char effectName[MAX_NAME_LENGTH];
 	int effectNum;
 }EFFECT;
 
 typedef struct person
 {
-	char personName[10];
+	char personName[MAX_NAME_LENGTH];
 	char personSex;
 	float personMoney;
 	EFFECT effect[MAX_COMMODITY_KIND];
@@ -32,7 +35,7 @@ COMMODITY commodity[MAX_COMMODITY_KIND] = {
 };
 
 //Person initial
-PERSON person = { "Hello",'M',10000,{0,"",0} };
+PERSON person = { "Hello",'M',10000,{0," ",0} };
 
 
 void Initialize()
@@ -43,26 +46,88 @@ void Initialize()
 	scanf_s("%s", &person.personName, 10);
 }
 
+int Settle(int order,int num)
+{
+	int total_price = order * num;
+	if (total_price > person.personMoney)
+	{
+		//ErrorCode:1.
+		//The user don`t have enough money.
+		return 1;
+	}
+	else
+	{
+		person.personMoney -= total_price;
+		//Looking for the user`s effect.
+		for (int i = 0; i < MAX_COMMODITY_KIND; i++)
+		{
+			//The user`s effect have the same commodity.
+			if (person.effect[i].effectID == commodity[order].commodityID)
+			{
+				person.effect[i].effectNum += num;
+				break;
+			}
+			//The user`s effect don`t have the same commodity.
+			if (person.effect[i].effectID == 0)
+			{
+				person.effect[i].effectID = commodity[order].commodityID;
+				//Transfer the commodity`s name.
+				for (int j = 0; j < MAX_NAME_LENGTH; j++)
+				{
+					if (commodity[order].commodityName[j] == NULL)break;
+					person.effect[i].effectName[j] = commodity[order].commodityName[j];
+				}
+				person.effect[i].effectNum = num;
+				break;
+			}
+		}
+		//Trade successfully.
+		return 0;
+	}
+}
+
 void Store()
 {
-	printf_s("ID   Name   Price   Number\n");
+	int choose = 0,order=0;
+	int  num= 0;
+	int result = 0;
+	printf_s("Welcome to the store.\n");
+	printf_s("You can buy what you want and what you can.\n");
+	printf_s("\n");
+	printf_s("Order  ID   Name    Price   Number\n");
 	for (int i = 0; i < MAX_COMMODITY_KIND; i++)
 	{
-		if (commodity[i].commodityID == 0)
-		{
-			break;
-		}
+		if (commodity[i].commodityID == 0)break;
+		printf_s("%d", i+1);
 		printf_s("%d  ", commodity[i].commodityID);
-		printf_s("%s  ", commodity[i].commodityName); 
+		printf_s("%s   ", commodity[i].commodityName); 
 		printf_s("%f  ", commodity[i].commodityPrice);
 		printf_s("%d", commodity[i].commodityNum);
 		printf_s("\n");
+	}
+	printf_s("If you want to buy something please the order, otherwise please input 0 and exit the store:\n");
+	if (choose != 0)
+	{
+		printf_s("Please input the number:\n");
+		scanf_s("%d", &num);		order = choose - 1;
+		//TODO: Check the input		
+		result=Settle(order, num);
+		switch (result)
+		{
+		case 0:printf_s("Successful!\n"); break;
+		case 1:printf_s("Failed, you don`t have enough money.\n"); break;
+		}
+	}
+	else if (choose == 0)
+	{
+		printd_s("Looking forward to your next visit.");
 	}
 }
 
 void Info()
 {
 	printf_s("Name:%s\n", person.personName);
+	//TODO: Using switch()
 	if (person.personSex == 'M')
 	{
 		printf_s("Sex:Male\n");
